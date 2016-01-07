0820450af87930ab24de35df2aec03c71ee69bcb
Pavel Djundik <github@xpaw.me>
2016-01-08 01:10:48 +0200

    Swallow mysql exceptions

--- SteamWebPipes/Events/ChangelistEvent.cs
+++ SteamWebPipes/Events/ChangelistEvent.cs
@@ -22,26 +22,33 @@ namespace SteamWebPipes
         {
             ChangeNumber = callback.CurrentChangeNumber;
 
-            if (Bootstrap.DatabaseConnectionString != null && callback.AppChanges.Any())
+            if (callback.AppChanges.Any())
             {
                 Apps = callback.AppChanges.ToDictionary(x => x.Key, x => "Unknown App");
 
-                var apps = string.Join(",", callback.AppChanges.Keys);
-
-                using (var reader = MySqlHelper.ExecuteReader(Bootstrap.DatabaseConnectionString, "SELECT `AppID`, `Name`, `LastKnownName` FROM `Apps` WHERE `AppID` IN(" + apps + ")"))
+                try
                 {
-                    while (reader.Read())
+                    var apps = string.Join(",", callback.AppChanges.Keys);
+
+                    using (var reader = MySqlHelper.ExecuteReader(Bootstrap.DatabaseConnectionString, "SELECT `AppID`, `Name`, `LastKnownName` FROM `Apps` WHERE `AppID` IN(" + apps + ")"))
                     {
-                        var name = reader.GetString(1);
-                        var lastKnownName = reader.GetString(2);
-
-                        if (!string.IsNullOrEmpty(lastKnownName) && name != lastKnownName)
+                        while (reader.Read())
                         {
-                            name = string.Format("{0} ({1})", name, lastKnownName);
-                        }
+                            var name = reader.GetString(1);
+                            var lastKnownName = reader.GetString(2);
 
-                        Apps[reader.GetUInt32(0)] = name;
+                            if (!string.IsNullOrEmpty(lastKnownName) && name != lastKnownName)
+                            {
+                                name = string.Format("{0} ({1})", name, lastKnownName);
+                            }
+
+                            Apps[reader.GetUInt32(0)] = name;
+                        }
                     }
+                }
+                catch (MySqlException e)
+                {
+                    Bootstrap.Log("{0}", e.Message);
                 }
             }
             else
@@ -49,18 +56,25 @@ namespace SteamWebPipes
                 Apps = new Dictionary<uint, string>();
             }
 
-            if (Bootstrap.DatabaseConnectionString != null && callback.PackageChanges.Any())
+            if (callback.PackageChanges.Any())
             {
                 Packages = callback.PackageChanges.ToDictionary(x => x.Key, x => "Unknown Package");
 
-                var subs = string.Join(",", callback.PackageChanges.Keys);
-
-                using (var reader = MySqlHelper.ExecuteReader(Bootstrap.DatabaseConnectionString, "SELECT `SubID`, `LastKnownName` FROM `Subs` WHERE `SubID` IN(" + subs + ")"))
+                try
                 {
-                    while (reader.Read())
+                    var subs = string.Join(",", callback.PackageChanges.Keys);
+
+                    using (var reader = MySqlHelper.ExecuteReader(Bootstrap.DatabaseConnectionString, "SELECT `SubID`, `LastKnownName` FROM `Subs` WHERE `SubID` IN(" + subs + ")"))
                     {
-                        Packages[reader.GetUInt32(0)] = reader.GetString(1);
+                        while (reader.Read())
+                        {
+                            Packages[reader.GetUInt32(0)] = reader.GetString(1);
+                        }
                     }
+                }
+                catch (MySqlException e)
+                {
+                    Bootstrap.Log("{0}", e.Message);
                 }
             }
             else
